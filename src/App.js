b5281edb24856c0015da47af80976cb57f973c60
ali abbani <aliox@ahlam.com>
2022-04-15 16:34:25 +0300

    Update the code upon mr sebastien request

--- src/App.js
+++ src/App.js
@@ -2,42 +2,29 @@ import { useState, useEffect } from 'react';
 import './app.css';
 
 function App() {
-  const [query, setQuery] = useState([]);
+  const [hits, setHits] = useState([]);
   const [input, setInput] = useState("");
-
-  const handleChange = (event) => {
-    setInput(event.target.value);
-  };
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-        'https://pixabay.com/api/?key=26680467-f23804cff8e6e49ca4c6bbb4c&q=all&image_type=photo'
+        `https://pixabay.com/api/?key=26680467-f23804cff8e6e49ca4c6bbb4c&q=${input}&image_type=photo`
       );
       const data = await response.json();
-      const { hits } = data;
-      setQuery(() => hits);
+      setHits(data.hits);
     }
     fetchData();
-  }, []);
+  }, [input]);
 
   return (
     <div className="pt-10 px-2 pb-20">
       <input
         className="input-idea pl-2 mb-5 w-3/5 h-9"
         placeholder="search ..."
-        onChange={handleChange}
+        onChange={(e) => setInput(e.target.value)}
       />
       <div className="sm:flex flex-row flex-wrap gap-3 ">
-        {query.filter((post) => {
-            if (query === '') {
-              return post;
-            } else if (post.tags.toLowerCase().includes(input.toLowerCase())) {
-              return post;
-            }
-            return false;
-          })
-          .map((post) => (
+        {hits.map((post) => (
             <div key={post.id}>
               <img
                 className="image-flower pb-3 sm:pb-0 "
